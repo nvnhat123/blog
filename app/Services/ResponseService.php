a44361bdbeaf4d1280db8c7844f216015c3f2b70
nvnhat123 <nhat.nguyen@pirago.work>
2022-10-28 10:38:14 +0700

    feature: add response service

---
+++ app/Services/ResponseService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Response as ResponseStatus;
+use Illuminate\Http\JsonResponse;
+
+class ResponseService
+{
+    private array $response;
+
+    public function __construct()
+    {
+        $this->response = [];
+        $this->setMsg('OK');
+        $this->setErrMsg('');
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->response['status'] = $status;
+
+        return $this;
+    }
+
+    public function setMsg(string $msg): self
+    {
+        $this->response['msg'] = $msg;
+
+        return $this;
+    }
+
+    public function setErrMsg(string $errMsg): self
+    {
+        $this->payload['error_messages'] = $errMsg;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $data
+     * @return self
+     */
+    public function setData($data = []): self
+    {
+        $this->payload['data'] = $data;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $data
+     * @return JsonResponse
+     */
+    public function getSuccess($data = []): JsonResponse
+    {
+        return $this->setStatus(ResponseStatus::HTTP_OK)->setData($data)->get();
+    }
+
+    public function get(): JsonResponse
+    {
+        return response()->json($this->response);
+    }
+
+}
--- app/helper.php
+++ app/helper.php
@@ -1,1 +1,11 @@
 <?php
+
+use App\Services\ResponseService;
+use Illuminate\Http\JsonResponse;
+
+if (! function_exists('responder')) {
+    function responder(): JsonResponse
+    {
+        return app(ResponseService::class);
+    }
+}
